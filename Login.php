40c6d7639ee50cc639eb58e25f24fcd5edade57f
prash_98 <prashant.iyer98@gmail.com>
2017-07-20 21:29:12 +0530

    Login page

---
+++ Login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="frm">
+		<form action="process.php" method="POST">
+			<p>
+				<label>Username:</label>
+				<input type="text" id="user" name="user" />
+			</p>
+			<p>
+				<label>Password:</label>
+				<input type="Password" id="Pass" name="Pass" />
+			</p>
+			<p>
+				<input type="submit" id="btn" value="Login" />
+			</p>
+		</form>
+		
+	</div>
+	
+</body>
